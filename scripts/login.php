7c5594cbf98a6bd3d54818938b92ad777c2b88a8
Jake Samuels <crazy_jake74@yahoo.com>
2019-04-22 18:20:59 -0500

    login issue fixed

--- scripts/login.php
+++ scripts/login.php
@@ -3,13 +3,16 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
+$dbname  = "Hoteldb";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
 
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST')

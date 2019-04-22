00cfec13416fa831d6ab022bc15df93ae93ab94f
Jake Samuels <crazy_jake74@yahoo.com>
2019-04-22 01:27:10 -0500

    form and php files updated for booking

--- scripts/booking.php
+++ scripts/booking.php
@@ -1,5 +1,22 @@
 <?php
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST')
+{
+    if(isset($_POST['finish']))
+    {
+        $fullname  = explode(" ", $_POST['fullname']); 
+        $firstname = $fullname[0];
+        $lastname  = $fullname[1];
+        $email     = $_POST['email'];
+        $adults    = $_POST['numadlts'];
+        $children  = $_POST['numchldrn'];
+        $streetnu  = $_POST['streetname'];
+        $phone     = $_POST['phone'];
+        $startdate = $_POST['startdate'];
+        $enddate   = $_POST['enddate'];
+        $suitetype = $_POST['suitetype'];
+    }
+}
 
 
 ?>
